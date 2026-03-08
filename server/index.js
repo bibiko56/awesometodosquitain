563ffae0f7d2d96fd849b0e754a3a4a311debb2a
RhayVent <quitainrhayvent@gmail.com>
2026-03-08 23:17:59 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const { connectToMongoDB } = require("./database");
+const path = require('path');
+
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const router = require("./routes");
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
+
+const startServer = async () => {
+    await connectToMongoDB();
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    });
+};
+startServer();
